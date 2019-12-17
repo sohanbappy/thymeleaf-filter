b5f0e8640e25b9f08b82d1ea61b66241bea4426a
bappy <sohanbappy@gmail.com>
2019-12-17 13:32:54 +0600

    Interceptor added

--- src/main/java/com/thyme/thymepagination/config/CustomizedFilterConfig.java
+++ src/main/java/com/thyme/thymepagination/config/CustomizedFilterConfig.java
@@ -1,7 +1,6 @@
 package com.thyme.thymepagination.config;
 
 import com.thyme.thymepagination.filters.UserFilter;
-import org.hibernate.internal.FilterConfiguration;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,11 +9,11 @@ import org.springframework.context.annotation.Configuration;
 public class CustomizedFilterConfig {
 
     @Bean
-    public FilterRegistrationBean<UserFilter> registrationBean(){
-        FilterRegistrationBean<UserFilter> regBean = new FilterRegistrationBean<UserFilter>();
-        regBean.setFilter(new UserFilter());
-        regBean.addUrlPatterns("/user/add/*");
-        return regBean;
+    public FilterRegistrationBean<UserFilter> logFilter() {
+        FilterRegistrationBean<UserFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new UserFilter());
+        registrationBean.addUrlPatterns("/user/add/*");
+        return registrationBean;
     }
 
 }
--- src/main/java/com/thyme/thymepagination/filters/UserFilter.java
+++ src/main/java/com/thyme/thymepagination/filters/UserFilter.java
@@ -2,6 +2,7 @@ package com.thyme.thymepagination.filters;
 
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
@@ -10,16 +11,12 @@ import java.io.IOException;
 
 @Component
 @Order(1) //ordering is needed when multiple filters are applied
-public class UserFilter implements Filter {
+public class UserFilter extends OncePerRequestFilter {
+
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
-
-
-        HttpServletRequest req = (HttpServletRequest) servletRequest;
-        HttpServletResponse res = (HttpServletResponse) servletResponse;
-        System.out.println("User filter is working From........"+req.getMethod()+" "+req.getRequestURI());
-
+    protected void doFilterInternal(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws ServletException, IOException {
+        String name = req.getParameter("name");
+        System.out.println("User filter is working From........"+req.getMethod()+" "+req.getRequestURI()+" and Name is: "+name);
         chain.doFilter(req,res);
-
     }
 }
